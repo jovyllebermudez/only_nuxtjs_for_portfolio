c5141799fdf31850fec6141966f1d0d752e05b8a
Jovylle Bermudez CMD <jovyllebermude.z@gmail.com>
2022-09-30 13:38:59 +0800

    udpate:make custom color in extend, white bg on card

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="aspect-square rounded-lg shadow-lg w-[280px] sm:w-[350px] m-8">
+  <div class="aspect-square rounded-lg shadow-lg w-[280px] sm:w-[350px] m-8 bg-white">
     <!-- <img src="/hero_img.jpeg" alt="asda"> -->
     <Website class="max-h-full max-w-full" />
     <div class="py-1 sm:py-3">
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,22 +9,23 @@ module.exports = {
     // "./nuxt.config.{js,ts}",
   ],
   theme: {
-    colors: {
-      "primary-light": "#F7F8FC",
-      "secondary-light": "#FFFFFF",
-      "ternary-light": "#f6f7f8",
+    extend: {
+      colors: {
+        "primary-light": "#F7F8FC",
+        "secondary-light": "#FFFFFF",
+        "ternary-light": "#f6f7f8",
 
-      "primary-dark": "#0D2438",
-      "secondary-dark": "#102D44",
-      "ternary-dark": "#1E3851",
+        "primary-dark": "#0D2438",
+        "secondary-dark": "#102D44",
+        "ternary-dark": "#1E3851",
 
-      "m1": "#5D5C61",
-      "m2": "#379683",
-      "m3": "#7395AE",
-      "m4": "#557A95",
-      "m5": "#B1A296"
+        "m1": "#5D5C61",
+        "m2": "#379683",
+        "m3": "#7395AE",
+        "m4": "#557A95",
+        "m5": "#B1A296"
+      },
     },
-    extend: {},
   },
   plugins: [],
 }
