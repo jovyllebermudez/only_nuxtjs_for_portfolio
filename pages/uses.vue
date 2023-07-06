3aa4a6afefee08adc491996dc6be8c983d05c868
Jovylle Bermudez CMD <jovyllebermude.z@gmail.com>
2023-07-07 01:25:57 +0800

    remove darkmode styles tailwind on uses page

--- pages/uses.vue
+++ pages/uses.vue
@@ -6,7 +6,7 @@
           <h2
             class="font-general-medium
               text-2xl text-primary-dark
-              dark:text-primary-light
+              
               mt-8
               mb-8
             "
@@ -15,73 +15,61 @@
           </h2>
           <ul class="font-general-regular">
             <li class="flex">
-              <i
-                class="bx bx-laptop text-gray-500 dark:text-gray-400 mr-4 text-2xl"
-              ></i>
+              <i class="bx bx-laptop text-gray-500  mr-4 text-2xl"></i>
               <a
                 href="#"
                 aria-label="Website and Phone"
-                class="text-lg mb-4 text-ternary-dark dark:text-ternary-light"
+                class="text-lg mb-4 text-ternary-dark "
               >
                 Macbook Pro 14" 2021 M1 Pro for hustle and bustle
               </a>
             </li>
             <li class="flex">
-              <i
-                class="bx bx-laptop text-gray-500 dark:text-gray-400 mr-4 text-2xl"
-              ></i>
+              <i class="bx bx-laptop text-gray-500  mr-4 text-2xl"></i>
               <a
                 href="#"
                 aria-label="Website and Phone"
-                class="text-lg mb-4 text-ternary-dark dark:text-ternary-light"
+                class="text-lg mb-4 text-ternary-dark "
               >
                 Ryzen 5 3600 16GB RAM 500GB SSD For Gaming and Streaming
               </a>
             </li>
             <li class="flex">
-              <i
-                class="bx bx-laptop text-gray-500 dark:text-gray-400 mr-4 text-2xl"
-              ></i>
+              <i class="bx bx-laptop text-gray-500  mr-4 text-2xl"></i>
               <a
                 href="#"
                 aria-label="Website and Phone"
-                class="text-lg mb-4 text-ternary-dark dark:text-ternary-light"
+                class="text-lg mb-4 text-ternary-dark "
               >
                 A4Tech FBX51C for Convenient Wireless Keyboard
               </a>
             </li>
             <li class="flex">
-              <i
-                class="bx bx-mouse-alt text-gray-500 dark:text-gray-400 mr-4 text-2xl"
-              ></i>
+              <i class="bx bx-mouse-alt text-gray-500  mr-4 text-2xl"></i>
               <a
                 href="#"
                 aria-label="Website and Phone"
-                class="text-lg mb-4 text-ternary-dark dark:text-ternary-light"
+                class="text-lg mb-4 text-ternary-dark "
               >
                 UGREEN Mouse4000 for Wireless Mouse
               </a>
             </li>
             <li class="flex">
-              <i
-                class="bx bx-headphone text-gray-500 dark:text-gray-400 mr-4 text-2xl"
-              ></i>
+              <i class="bx bx-headphone text-gray-500  mr-4 text-2xl"></i>
               <a
                 href="#"
                 aria-label="Website and Phone"
-                class="text-lg mb-4 text-ternary-dark dark:text-ternary-light"
+                class="text-lg mb-4 text-ternary-dark "
               >
                 A4Tech HU-10 - plain and classic headset
               </a>
             </li>
             <li class="flex">
-              <i
-                class="bx bx-baguette text-gray-500 dark:text-gray-400 mr-4 text-2xl"
-              ></i>
+              <i class="bx bx-baguette text-gray-500  mr-4 text-2xl"></i>
               <a
                 href="#"
                 aria-label="Website and Phone"
-                class="text-lg mb-4 text-ternary-dark dark:text-ternary-light"
+                class="text-lg mb-4 text-ternary-dark "
               >
                 LOTTE Xylitol Sugar Free Chewing Gum XD to manaage anxiety
               </a>
@@ -92,7 +80,7 @@
           <h2
             class="font-general-medium
               text-2xl text-primary-dark
-              dark:text-primary-light
+              
               mt-8
               mb-8
             "
@@ -101,49 +89,42 @@
           </h2>
           <ul class="font-general-regular">
             <li class="flex">
-              <i
-                class="bx bx-code-alt text-gray-500 dark:text-gray-400 mr-4 text-2xl"
-              ></i>
+              <i class="bx bx-code-alt text-gray-500  mr-4 text-2xl"></i>
               <a
                 href="#"
                 aria-label="Website and Phone"
-                class="text-lg mb-4 text-ternary-dark dark:text-ternary-light"
+                class="text-lg mb-4 text-ternary-dark "
               >
                 VSCode IDE using atom dark theme
               </a>
             </li>
             <li class="flex">
-              <i
-                class="bx bx-code-alt text-gray-500 dark:text-gray-400 mr-4 text-2xl"
-              ></i>
+              <i class="bx bx-code-alt text-gray-500  mr-4 text-2xl"></i>
               <a
                 href="#"
                 aria-label="Website and Phone"
-                class="text-lg mb-4 text-ternary-dark dark:text-ternary-light"
+                class="text-lg mb-4 text-ternary-dark "
               >
                 MacOS Ventura 13.0 OS trying to keep using latest OS updates
               </a>
             </li>
             <li class="flex">
-              <i
-                class="bx bx-list-check text-gray-500 dark:text-gray-400 mr-4 text-2xl"
-              ></i>
+              <i class="bx bx-list-check text-gray-500  mr-4 text-2xl"></i>
               <a
                 href="#"
                 aria-label="Website and Phone"
-                class="text-lg mb-4 text-ternary-dark dark:text-ternary-light"
+                class="text-lg mb-4 text-ternary-dark "
               >
-                Airtable TodoList System - amazingly helpful for lazy guy like me
+                Airtable TodoList System - amazingly helpful for lazy guy like
+                me
               </a>
             </li>
             <li class="flex">
-              <i
-                class="bx bx-terminal text-gray-500 dark:text-gray-400 mr-4 text-2xl"
-              ></i>
+              <i class="bx bx-terminal text-gray-500  mr-4 text-2xl"></i>
               <a
                 href="#"
                 aria-label="Website and Phone"
-                class="text-lg mb-4 text-ternary-dark dark:text-ternary-light"
+                class="text-lg mb-4 text-ternary-dark "
               >
                 ZSH Terminal or VSCODE Terminal - sufficient for me
               </a>
