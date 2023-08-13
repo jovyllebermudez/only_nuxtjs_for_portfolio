f10f1a21386116af26a544ddd2a5e0336f08f65a
Jovylle Bermudez CMD <jovyllebermude.z@gmail.com>
2023-08-13 16:18:57 +0800

    home icon

--- pages/index.vue
+++ pages/index.vue
@@ -3,12 +3,12 @@
     <section class="md:h-[80vh] flex_center py-8 ">
       <div class="container mx-auto block sm:flex_center">
         <div class="px-10">
-          <nuxt-img
+          <img
             class="rounded-2xl max-h-[500px] my_hover"
             src="/me_face.jpg"
             alt="programmer person"
-            loading="lazy" 
           />
+          
         </div>
         <div class="text-center px-10 py-10 space-y-4">
           <h1 class="text-4xl font-semibold uppercase my_hover">
@@ -18,34 +18,35 @@
             <i class="bx bxs-hot"></i>
             <i class="bx bxl-facebook-square"></i> -->
           </h1>
-          <p class="text-m4 py-3">A Full-Stack Web Developer</p>
+          <p class="text-m4 py-3">
+            A Full-Stack Web Developer
+          </p>
           <HelloSvg class="w-[200px] h-[100px] mx-auto" />
-          <a class="" href="#">
-            <button
-              class="hover:line-through my_hover hover:cursor-default rounded-lg border-2 border-m1 my-4 py-1 px-3 text-base"
-            >
+          <a
+            class=""
+            href="#"
+          >
+            <button class="hover:line-through my_hover hover:cursor-default rounded-lg border-2 border-m1 my-4 py-1 px-3 text-base">
               See my CV / Resume
-              <i class="bx bxs-right-top-arrow-circle"></i>
+              <i class='bx bxs-right-top-arrow-circle'></i>
             </button>
           </a>
         </div>
       </div>
     </section>
-    <section class="md:min-h-[60vh] flex_center" id="about">
+    <section
+      class="md:min-h-[60vh] flex_center"
+      id="about"
+    >
       <div class="container text-center ">
-        <h2
-          class="text-3xl sm:text-5xl text-center font-semibold my-8 my_hover"
-        >
+        <h2 class="text-3xl sm:text-5xl text-center font-semibold my-8 my_hover">
           About me
         </h2>
         <div>
-          <Coding class="h-[200px] mx-auto" />
+          <Coding class="h-[200px] mx-auto max-w-full" />
         </div>
         <p class="my_hover">
-          I, Jovylle, can help you to build or maintain your system on either
-          Front End or Back End. Through the years of working in this actively
-          changing field, I have experienced a good quantity of challenges that
-          equips me with the skills to be one of your best assets.
+          I, Jovylle, can help you to build or maintain your system on either Front End or Back End. Through the years of working in this actively changing field, I have experienced a good quantity of challenges that equips me with the skills to be one of your best assets.
         </p>
       </div>
     </section>
@@ -68,43 +69,42 @@
             </button>
           </a>
         </div> -->
-    <!-- <p>Search projects by title or by text from description</p> -->
-    <!-- <div class="flex flex-wrap justify-center">
+        <!-- <p>Search projects by title or by text from description</p> -->
+        <!-- <div class="flex flex-wrap justify-center">
           <ProjectCard class="my_hover" />
           <ProjectCard class="my_hover" />
           <ProjectCard class="my_hover" />
         </div> -->
-    <!-- <a href="https://alimyon.dev/">
+        <!-- <a href="https://alimyon.dev/">
           <button class="my-8 my_hover rounded-lg bg-m4 text-primary-light py-4 px-8">
             See All
           </button>
         </a> -->
-    <!-- </div>
+      <!-- </div>
     </section> -->
-    <section class="md:h-[60vh] flex_center" id="contact">
+    <section
+      class="md:h-[60vh] flex_center"
+      id="contact"
+    >
       <div class="container my-10">
-        <h2
-          class="text-3xl sm:text-5xl text-center text-primary-dark font-semibold my-8 my_hover"
-        >
+        <h2 class="text-3xl sm:text-5xl text-center text-primary-dark font-semibold my-8 my_hover">
           Contact Me / Socials
         </h2>
-        <div
-          class="flex justify-center space-x-6 my-8 items-center text-4xl text-m1"
-        >
+        <div class="flex justify-center space-x-6 my-8 items-center text-4xl text-m1">
           <a href="https://www.facebook.com/twero">
-            <i class="my_hover bx bxl-facebook-circle"></i>
+            <i class='my_hover bx bxl-facebook-circle'></i>
           </a>
           <a href="mailto:jovyllebermudez@gmail.com">
-            <i class="my_hover bx bxl-gmail"></i>
+            <i class='my_hover bx bxl-gmail'></i>
           </a>
           <a href="https://github.com/jovyllebermudez">
-            <i class="my_hover bx bxl-github"></i>
+            <i class='my_hover bx bxl-github'></i>
           </a>
           <!-- <a href="">
             <i class='my_hover bx bxl-dev-to'></i>
           </a> -->
           <a href="https://www.linkedin.com/in/jovyllebermudez/">
-            <i class="my_hover bx bxl-linkedin"></i>
+            <i class='my_hover bx bxl-linkedin'></i>
           </a>
         </div>
       </div>
@@ -142,4 +142,5 @@ useHead({
   meta: [{ name: 'description', content: 'Genuinely addicted to coding.' }]
 });
 </script>
-<style></style>
+<style>
+</style>
